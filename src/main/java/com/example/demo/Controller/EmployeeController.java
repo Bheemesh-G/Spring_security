acdce7a4eb12de4464a0a2b7c4b8ab2febbfec08
Bheemesh <gbheemesh888@gmail.com>
2023-04-21 15:36:42 +0530

    test2 with keycloak

--- src/main/java/com/example/demo/Controller/EmployeeController.java
+++ src/main/java/com/example/demo/Controller/EmployeeController.java
@@ -1,11 +1,15 @@
 package com.example.demo.Controller;
 
+import javax.annotation.security.RolesAllowed;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,23 +19,25 @@ import com.example.demo.Modules.Employee;
 import com.example.demo.Service.EmployeeService;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/api")
 public class EmployeeController {
 	
 	
+	
+//@RequestHeader String Authorization -- is used to expect a token	
 	
 	@Autowired
 	EmployeeService employeeService;
 	
 	
 	@PostMapping("/add-employee")
-	public String addEmployee(@RequestBody EmployeeRequest employeeRequest)
+	public String addEmployee(@RequestBody EmployeeRequest employeeRequest,@RequestHeader String Authorization)
 	{
 		return employeeService.addEmployee(employeeRequest);
 	}
 	
 	
-	@GetMapping("/get-employee")
+	@GetMapping("/get/employee")
 	public Employee getEmployee(@RequestParam int id)
 	{
 		return employeeService.getEmployee(id);
@@ -45,14 +51,14 @@ public class EmployeeController {
 	}
 	
 	
-	@PutMapping("/update-password/{id}")
+	@PutMapping("/employee/update/{id}")
 	public String updatePassword(@PathVariable int id, @RequestParam  String password)
 	{
 		return employeeService.updatePassword(id, password);
 	}
 	
 	
-	@PutMapping("/update-role/{id}")
+	@PutMapping("/employee/update-role/{id}")
 	public String updateRole(@PathVariable int id, @RequestParam String role) {
 		return employeeService.updateRole(id, role);
 	}
--- src/main/java/com/example/demo/SpringSecurityConfig/SpringSecurityConfigration.java
+++ src/main/java/com/example/demo/SpringSecurityConfig/SpringSecurityConfigration.java
@@ -1,52 +1,53 @@
-package com.example.demo.SpringSecurityConfig;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-import com.example.demo.Service.CustomEmployeeDetailService;
-
-@Configuration
-@EnableWebSecurity
-public class SpringSecurityConfigration {
-	
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder()
-	{
-		return new BCryptPasswordEncoder();
-	}
-	
-	
-	@Autowired
-	private CustomEmployeeDetailService customEmployeeDetailService;
-	
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
-		
-		httpSecurity.csrf().disable()
-		            .authorizeHttpRequests()
-		            .requestMatchers("/employee/home").permitAll()
-		            .requestMatchers("/employee/add-employee").hasRole("FRONTEND")
-		            .anyRequest()
-		            .authenticated()
-		            .and()
-		            .httpBasic();
-		            
-		            
-		     return httpSecurity.build();       
-		
-		
-	}
-	
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(customEmployeeDetailService).passwordEncoder(passwordEncoder());
-		
-	}
-
-}
+//package com.example.demo.SpringSecurityConfig;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import com.example.demo.Service.CustomEmployeeDetailService;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity
+//public class SpringSecurityConfigration {
+//	
+//	@Bean
+//	public BCryptPasswordEncoder passwordEncoder()
+//	{
+//		return new BCryptPasswordEncoder();
+//	}
+//	
+//	
+//	@Autowired
+//	private CustomEmployeeDetailService customEmployeeDetailService;
+//	
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+//		
+//		httpSecurity.csrf().disable()
+//		            .authorizeHttpRequests()
+//		            .requestMatchers("/employee/home").permitAll()
+//		            .anyRequest()
+//		            .authenticated()
+//		            .and()
+//		            .httpBasic();
+//		            
+//		            
+//		     return httpSecurity.build();       
+//		
+//		
+//	}
+//	
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.userDetailsService(customEmployeeDetailService).passwordEncoder(passwordEncoder());
+//		
+//	}
+//
+//}
